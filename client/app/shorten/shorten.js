8cfd5ec64f9c2bf380e0cd56a9c68de7ea2e2573
MrJonWu <mrjonwu@gmail.com>
2016-12-03 15:08:04 -0800

    Implement search

--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -5,13 +5,29 @@ angular.module('shortly.shorten', [])
 
   var rValidUrl = /^(?!mailto:)(?:(?:https?|ftp):\/\/)?(?:\S+(?::\S*)?@)?(?:(?:(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[0-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))|localhost)(?::\d{2,5})?(?:\/[^\s]*)?$/i;
 
-  $scope.isValid = rValidUrl;
+  $scope.validator = rValidUrl;
 
-  $scope.submitForm = function(isValid) {
-    if (isValid) {
-      console.log('valid');
-      $scope.addLink();
-    }
+  $scope.submitForm = function($event) {
+    //if (isValid) {
+    //console.log($scope);
+
+    $scope.addLink();
+    // if (!$scope.link.url.match(rValidUrl)) {
+    //   $scope.urlForm.$invalid = true;
+    //   $scope.urlForm.$valid = false;
+    // } else {
+    //   $scope.addLink();
+    // }
+
+    // if ($scope.urlForm.$invalid) {
+    //   $event.preventDefault();
+    // } else {
+    //   $scope.addLink();
+    // }
+      
+    // } else {
+    //   return false;
+    // }
   };
 
   $scope.addLink = function() {
