e92f61417e124076d9cf0a9a4df5a6119ea62380
MrJonWu <mrjonwu@gmail.com>
2016-12-02 19:41:56 -0800

    Implement URL shortener

--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -2,7 +2,13 @@ angular.module('shortly.shorten', [])
 
 .controller('ShortenController', function ($scope, $location, Links) {
   $scope.link = {};
-  $scope.addLink = function() {
+
+  $scope.addLink = function(link) {
+    $scope.link.url = link;
     Links.addOne($scope.link);
   };
+  // $shortUrlField 
+  // $scope.submit = function() {
+
+  // };
 });
