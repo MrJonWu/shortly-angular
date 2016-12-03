5284bd5aacc6c1a9ed2c8cb898c2afdf0be2c83e
MrJonWu <mrjonwu@gmail.com>
2016-12-03 12:35:20 -0800

    Modify validation to use angular ng-pattern

--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -5,8 +5,21 @@ angular.module('shortly.shorten', [])
 
   var rValidUrl = /^(?!mailto:)(?:(?:https?|ftp):\/\/)?(?:\S+(?::\S*)?@)?(?:(?:(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[0-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))|localhost)(?::\d{2,5})?(?:\/[^\s]*)?$/i;
 
+  $scope.isValid = rValidUrl;
+
+  $scope.submitForm = function(isValid) {
+    if (isValid) {
+      console.log('valid');
+      $scope.addLink();
+    }
+  };
+
   $scope.addLink = function() {
+
+    // console.log($scope.link.url.$valid);
+
     Links.addOne($scope.link);
+
     // if ($scope.link.url.indexOf('http://') === - 1) {
     //   $scope.link.url = 'http://' + $scope.link.url;
     // }
@@ -20,3 +33,8 @@ angular.module('shortly.shorten', [])
     // }    
   };
 });
+
+  // <form name='urlForm' ng-submit='submitForm(urlForm.$valid)' novalidate>
+  //     <input ng-model="link.url" class='text' type='url' name='url' required>
+  //     <input type='submit' value='Shorten'>
+  // </form>
