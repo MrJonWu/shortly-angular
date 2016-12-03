52fa86b89c902318f0184ac763479e8387909915
MrJonWu <mrjonwu@gmail.com>
2016-12-03 11:38:46 -0800

    Use HTML5 URL validation

--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -6,17 +6,17 @@ angular.module('shortly.shorten', [])
   var rValidUrl = /^(?!mailto:)(?:(?:https?|ftp):\/\/)?(?:\S+(?::\S*)?@)?(?:(?:(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[0-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))|localhost)(?::\d{2,5})?(?:\/[^\s]*)?$/i;
 
   $scope.addLink = function() {
-    if ($scope.link.url.indexOf('http://') === - 1) {
-      $scope.link.url = 'http://' + $scope.link.url;
-    }
-    if ($scope.link.url.match(rValidUrl)) {
-      console.log('VALID');
-      Links.addOne($scope.link);
-      // $scope.link.url = '';
-    } else {
-      console.log('INVALID URL');
-      $scope.link.url = 'PLEASE USE A VALID URL';
-    }
-    
+    Links.addOne($scope.link);
+    // if ($scope.link.url.indexOf('http://') === - 1) {
+    //   $scope.link.url = 'http://' + $scope.link.url;
+    // }
+    // if ($scope.link.url.match(rValidUrl)) {
+    //   console.log('VALID');
+    //   Links.addOne($scope.link);
+    //   // $scope.link.url = '';
+    // } else {
+    //   console.log('INVALID URL');
+    //   //$scope.link.url = 'PLEASE USE A VALID URL';
+    // }    
   };
 });
